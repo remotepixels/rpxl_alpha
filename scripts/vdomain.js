4783a735950c127060c6e90eb596699a8b517148
remotepixels <app+github@remotepixels.app>
2025-05-06 10:14:09 +0200

    check post and send function

--- scripts/vdomain.js
+++ scripts/vdomain.js
@@ -2,9 +2,10 @@
 window.addEventListener('message', (event) => {
     // Check if the message is from the expected origin
     if (event.origin === 'https://alpha.rpxl.app') {
-        console.log('Received message from parent:', event.data);
+
+        //console.log('Received message from parent:', event.data);
     }
 });
 
 // Use postMessage to send data to the parent
-window.parent.postMessage('Hello from iframe!', 'https://alpha.rpxl.app');
+window.parent.postMessage('Hello from iframe!', 'https://vdo.rpxl.app');
