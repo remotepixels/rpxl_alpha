6df5f0f1e2028c40f7617769d336f66e8bd610a6
remotepixels <app+github@remotepixels.app>
2025-06-02 12:28:44 +0200

    timing changes on vdomain.js

--- scripts/vdomain.js
+++ scripts/vdomain.js
@@ -1,33 +1,4 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// Listen for draw co-ordinates from parent window and send to all clients
-////////////////////////////////////////////////////////////////////////////////////////////////////
-/*window.addEventListener('message', function(e) {
-  // IMPORTANT: Check the origin of the message to ensure it's from a trusted source.
-  // For example, if you expect messages from 'https://parent.example.com':
-  // if (event.origin !== 'https://parent.example.com') {
-  //   return; // Ignore messages from unexpected origins
-  // }
-  
 
-
-  if (e.data && e.data.type === 'drawData') {
-    let { lastpoint, xoffset, yoffset, force, color, canvasWidth, canvasHeight } = e.data;
-      window.parent.postMessage({
-        sendData: 'remoteDrawdata',
-        lastPoint: lastpoint,
-        x: xoffset, 
-        y: yoffset, 
-        force: force,
-        color: color,
-        canvasWidth: canvasWidth,
-        canvasHeight: canvasHeight
-      }, '*');
-
-     console.log('draw data recieved from parent');
-
-  }
-});
-*/
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Send the size of the 'main video stream (called holder)' element to the parent window
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -46,7 +17,7 @@ function sendMainstreamSize() {
   }
   if (width === 0 || height === 0) {
     //don't send if there is no video element, try again in 3 seconds
-    setTimeout(function() { sendMainstreamSize(); }, 3000);  
+    setTimeout(function() { sendMainstreamSize(); }, 1000);  
     return; 
   } else {  
   // Send the message to the parent window
@@ -62,7 +33,7 @@ function sendMainstreamSize() {
 }
 
 //wait for 5 seconds after loaded to run the first time
-setTimeout(function() { sendMainstreamSize(); }, 5000); 
+setTimeout(function() { sendMainstreamSize(); }, 4000); 
 
 // run everytime the window is resized but throttled
 // This will ensure that the function is not called too often
