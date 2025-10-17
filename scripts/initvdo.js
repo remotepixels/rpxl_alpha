433c1419c3be446735abaf40c73ede35054439e9
remotepixels <app+github@remotepixels.app>
2025-10-17 11:14:45 +0200

    changes to direcot layouts

--- scripts/initvdo.js
+++ scripts/initvdo.js
@@ -35,6 +35,7 @@ function viewerStream () {
         console.log("Viewer settings changed, reloading...");
 
         storeSelectedDevicesUser(); //store new user only settings - initui.js
+
         deactivateUserTools(); //turn off tools while reloading frame - initui.js
 
         //reload the stored values and use to reload viewers frame
@@ -73,7 +74,7 @@ function viewerStream () {
             "&chroma=3c3c3c"+
             "&nomouseevents"+
             "&group=Client"+
-            "&css=https%3A%2F%2Falpha.rpxl.app%2Fstyles%2Fviewersstream.css"+
+            "&css=https%3A%2F%2Falpha.rpxl.app%2Fstyles%2FvdoViewer.css"+
             ""; 
 
         reactivateUserTools(); //reactivate tools - initui.js
@@ -104,7 +105,7 @@ function viewMainStream () {
         "&waitimage=https%3A%2F%2Falpha.rpxl.app%2Fimages%2FnosignalHD.png"+
         "&buffer=1000"+//adds a xms buffer
         "&showlist=0"+//hides the viewer list
-        "&css=https%3A%2F%2Falpha.rpxl.app%2Fstyles%2Fmainstream.css"+
+        "&css=https%3A%2F%2Falpha.rpxl.app%2Fstyles%2FvdoMain.css"+
         "&js=https%3A%2F%2Falpha.rpxl.app%2Fscripts%2Fvdomain.js"+
         ""; 
 
@@ -187,7 +188,7 @@ function startMainStream() {
                 "&denoise=0"+//turns off denoiser
                 "&ab=128"+//constant audio bitrate
                 "&waitimage=https%3A%2F%2Falpha.rpxl.app%2Fimages%2FnosignalHD.png"+
-                "&css=https%3A%2F%2Falpha.rpxl.app%2Fstyles%2Fmainstream.css"+
+                "&css=https%3A%2F%2Falpha.rpxl.app%2Fstyles%2FvdoMain.css"+
                 "&js=https%3A%2F%2Falpha.rpxl.app%2Fscripts%2Fvdomain.js"+
                 ""; 
 
@@ -198,5 +199,92 @@ function startMainStream() {
         }
     } else {
         console.log("Main stream settings unchanged, not reloading")
+    }
+}
+
+
+function viewerStreamDirector () {
+    let storedUserName = sessionStorage.getItem("username"); //retrieve username, camera and mic settings from storage
+    let storedCameraIndex = sessionStorage.getItem("cameraSourceIndex");
+    let storedMicIndex = sessionStorage.getItem("microphoneSourceIndex");
+
+    let currentUsername = document.getElementById("name").value.trim() || "Streamer"; //current values in form`
+    let sanitizedCurrentUserName = encodeURIComponent(currentUsername); 
+
+    let currentCamera = document.getElementById("cameraSource").selectedIndex;
+    let currentMic = document.getElementById("microphoneSource").selectedIndex;
+
+    if (!sanitizedCurrentUserName) {
+        document.getElementById("name").style.animation = "pulse 500ms";
+        setTimeout(() => { document.getElementById("name").style.animation = "none"; }, 500);
+        document.getElementById("name").focus();
+        return;
+    } 
+        
+    if (avatar == "") {
+        // pick a random avatar image
+        const numberArray = Array.from({ length: 43 }, (_, i) => String(i).padStart(3, '0'));
+        const randomNum = numberArray[Math.floor(Math.random() * numberArray.length)];
+        avatar = `${randomNum}.png`;
+    }
+    //compare form values with stored values, if they're different then reload the iframe
+    if ((sanitizedCurrentUserName != storedUserName) || (currentCamera != storedCameraIndex) || (currentMic != storedMicIndex)) {
+        if (!document.getElementById("viewersStream").classList.contains("hidden") ) { 
+            document.getElementById("viewersStream").classList.add("hidden"); 
+        }
+        document.getElementById("popupBG").classList.add("hidden"); 
+        viewersStream.contentWindow.postMessage({ close: true }, "*"); // hangup connection on video ninja
+        console.log("Viewer settings changed, reloading...");
+
+        storeSelectedDevicesUser(); //store new user only settings - initui.js
+        deactivateUserTools(); //turn off tools while reloading frame - initui.js
+
+        //reload the stored values and use to reload viewers frame
+        let sanitizedSessionID = sessionStorage.getItem("sessionID");   //retrieve session ID
+        let sanitizedUserName = sessionStorage.getItem("username"); //retrieve username, camera and mic settings from storage
+        let sanitizedCamera = sessionStorage.getItem("cameraDevice");
+        let sanitizedMicrophone = sessionStorage.getItem("microphoneDevice");
+
+        //if no video source is selected or the camera is disabled in the browser then set to connect as miconly
+        if ((sanitizedCamera == "0") || (sanitizedCamera == "disabled_in_browser") || (sanitizedCamera == null) || (sanitizedCamera == "null") ) {
+            var camSetup = "&avatar=https%3A%2F%2Falpha.rpxl.app%2Favatars%2F"+avatar+"&videodevice=0";//"&novideo&videodevice=0";
+        } else {
+            var camSetup = "&videodevice="+sanitizedCamera+"&videobitrate=64";
+        }
+
+        if ((sanitizedMicrophone == "0") || (sanitizedMicrophone == "disabled_in_browser") || (sanitizedMicrophone == null) || (sanitizedMicrophone == "null") ) {
+            var micSetup = "&audiodevice=0";
+        } else {
+            var micSetup = "&audiodevice="+sanitizedMicrophone;
+        }
+
+        document.getElementById("viewersStream").src = "https://alpha.rpxl.app/vdo/?room=RPXL_"+sanitizedSessionID+
+            "&cleanish"+
+            "&director"+
+            "&showlabels"+
+            "&label="+sanitizedUserName+camSetup+micSetup+
+            "&hidehome"+
+            "&style=6"+
+            "&meterstyle=1"+
+            "&webcam"+
+            "&notify"+
+            "&disablehotkeys"+
+            "&clearstorage"+
+            "&autostart"+
+            "&nocontrols"+
+            "&signalmeter"+
+            "&chroma=3c3c3c"+
+            "&nomouseevents"+
+            "&group=Client"+
+            "&css=https%3A%2F%2Falpha.rpxl.app%2Fstyles%2FvdoDirector.css"+
+            ""; 
+
+        reactivateUserTools(); //reactivate tools - initui.js
+        setTimeout(function(){   
+            document.getElementById("viewersStream").classList.remove("hidden");  //wait 1 second and show frame
+        },2000);
+    } else {
+        console.log("Viewer settings unchanged, not reloading")
+        closeDialog(settingsDialog, toolSettings);
     }
 }
