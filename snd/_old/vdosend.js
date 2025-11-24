82387cf2307cb9f40a36e91aed6627df1eef2696
remotepixels <app+github@remotepixels.app>
2025-11-24 14:21:45 +0100

    minor tweaks to chunk size and delay, added wakelock for background processing

--- snd/_old/vdosend.js
+++
@@ -1,8 +0,0 @@
-  window.parent.postMessage({
-      sendData: 'mainstreamSize', // Add a type to easily filter messages
-      width: LEwidth,
-      height: LEheight,
-      top: LEtop,
-      left: LEleft,
-      "type": "pcs"
-    }, '*'); // Use '*' for the target origin for simplicity should be sent to parent
